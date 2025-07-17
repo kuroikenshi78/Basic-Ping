5a7342b6b2a48a7ffef81fb427e16e138c039e00
kuroikenshi78 <101783712+kuroikenshi78@users.noreply.github.com>
2025-07-17 12:35:51 +0900

    Create main.sh
    Initial creation of file.

---
+++ main.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#### Variables
+hosts=$(cat ./hosts)
+hosts_count=$(cat ./hosts | grep -c "[A-Za-z\|0-9]")
+counter=1
+
+# Sets color for text
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+#### Pre-Actions/Files
+> ./down
+
+#### Script Execution
+for c in $hosts
+do
+
+    # Message and formatting.
+    clear
+    echo -e "${GREEN}Pinging Devices! Please be patient:${NC} ${YELLOW}$counter|$hosts_count${NC}"
+
+    # Main Script
+    ping -c 2 $c > ./temp # Pings device 2 times
+    if grep -i "0 received" ./temp > /dev/null; then
+        echo "$c" >> ./down
+    fi
+
+    # Increaeses counter
+    ((counter++))
+done
+
+#### Final Readout
+if [ -e ./down ]; then # Checks to see if file is empty
+    echo -e "${GREEN}All devices are online!${NC}"
+else
+    echo "${YELLOW}The following devices are down!${NC}"
+    cat ./down
+fi
