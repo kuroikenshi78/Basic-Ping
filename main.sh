870eb2a45f9bb5f9b41528d2626786a0481c60b8
kuroikenshi78 <101783712+kuroikenshi78@users.noreply.github.com>
2025-07-17 17:08:24 +0900

    Update main.sh
    - Added a dynamic progress bar!

--- main.sh
+++ main.sh
@@ -2,11 +2,11 @@
 
 #### Variables
 hosts=$(cat ./hosts)
-hosts_count=$(grep -c "[A-Za-z0-9]" ./hosts)
+hosts_count=$(cat ./hosts | grep -c "[A-Za-z0-9]")
 counter=1
+bar_width=50
 
 # Sets color for text
-RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
@@ -14,18 +14,26 @@ NC='\033[0m'
 #### Pre-Actions/Files
 > ./down
 
+#### Initial Message
+clear
+echo -e "${GREEN}Pinging Devices! Please be patient!${NC}"
+
 #### Script Execution
 for c in $hosts
 do
-
-    # Message and formatting.
-    clear
-    echo -e "${GREEN}Pinging Devices! Please be patient:${NC} ${YELLOW}$counter|$hosts_count${NC}"
 
     # Main Script
     if ! ping -c 2 -W 2 "$c" &> /dev/null; then
         echo "$c" >> ./down
     fi
+
+    # Dynamic Progress Bar
+    filled=$(( counter * bar_width / hosts_count ))
+    bar=$(printf '#%.0s' $(seq 1 $filled))
+    empty=$(( bar_width - filled ))
+    spaces=$(printf ' %.0s' $(seq 1 $empty))
+
+    printf "\r[%-50s] %d/%d" "$bar$spaces" "$counter" "$hosts_count"
 
     # Increaeses counter
     ((counter++))
@@ -33,6 +41,7 @@ done
 
 #### Final Readout
 if [ -s ./down ]; then # Checks to see if file is empty
+    echo ""
     echo -e "\n${YELLOW}The Following devices are down:${NC}"
     cat ./down
 else
